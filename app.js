2f35031e489bd3e85d58a0af659ccbbd7acd004f
Antonio Muto <antonio.muto09@gmail.com>
2025-04-06 15:41:19 +0000

    changhed port to 5000

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.listen(5000, () => {
-    console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 5000!')
 })
 
 app.get('/', (req, res) => {
